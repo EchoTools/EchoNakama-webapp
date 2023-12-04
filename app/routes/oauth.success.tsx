a89189d64f5110fb3ba43f903f1b6d9d67763720
Andrew Bates <a@sprock.io>
2023-12-03 21:36:33 -0600

    Fix discord oauth when in dev

--- app/routes/oauth.success.tsx
+++ app/routes/oauth.success.tsx
@@ -10,18 +10,26 @@ import { useEffect, useRef } from "react";
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   String(request);
   return json({});
+
 };
+
+
+//const OAUTH_REDIRECT_URL = process.env.OAUTH_REDIRECT_URL ?? '';
+
 
 const _sanitizeLinkCode = (linkCode: string) => {
   return linkCode.toUpperCase().replace(/[^ABCDEFGHJKLMNPQRSTUVWXYZ]/g, '').slice(0, 4);
 }
 
 export const action = async ({ request }: ActionFunctionArgs) => {
+  const SUCCESS_REDIRECT_URL = process.env.SUCCESS_REDIRECT_URL ?? "/";
+  const DISCORD_OAUTH2_URL = process.env.DISCORD_OAUTH2_URL ?? '';
+  const NAKAMA_API_URL = process.env.NAKAMA_API_URL ?? '';
+  const OAUTH_REDIRECT_URL = process.env.OAUTH_REDIRECT_URL ?? '';
+
   const formData = await request.formData();
   let linkCode = String(formData.get("linkCode"));
-  const discordCode = String(formData.get("discordCode"));
-  // use environment variable SUCCESS_REDIRECT_URL
-  const successRedirectUrl = process.env.SUCCESS_REDIRECT_URL ?? "/";
+  const oauthCode = String(formData.get("discordCode"));
   
   linkCode = _sanitizeLinkCode(linkCode);
   if (linkCode.length != 4) {
@@ -29,30 +37,34 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       { errors: { linkCode: "linkCode is invalid", discordCode: null } },
       { status: 400 },
     );
-  } else if (discordCode == null) {
+  } else if (oauthCode == null) {
     return json(
       { errors: { linkCode: null, discordCode: "discord code is invalid. retry oauth." } },
       { status: 400 },
     );
   }
 
-  const DISCORD_OAUTH2_URL = process.env.DISCORD_OAUTH2_URL ?? '';
-  const API_URL = process.env.API_URL ?? '';
-  const OAUTH_REDIRECT_URL = process.env.OAUTH_REDIRECT_URL ?? '';
-
-
-  const response = await fetch(API_URL, {
+  const response = await fetch(NAKAMA_API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ linkCode, discordCode }),
+    body: JSON.stringify({ 
+      linkCode, 
+      oauthCode: oauthCode,
+      oauthRedirectUrl: OAUTH_REDIRECT_URL,
+  }),
   });
 
   if (response.status === 200) {
-    return redirect(successRedirectUrl);
+    return redirect(SUCCESS_REDIRECT_URL);
   } else if (response.status === 401) {
-    return redirect(DISCORD_OAUTH2_URL);
+    console.error(`Redirecting to oauth url, because of error from nakama: ${response.statusText}`)
+    //return redirect(DISCORD_OAUTH2_URL);
+    return json(
+      { errors: { linkCode: null, discordCode: "discord code is invalid. retry oauth." } },
+      { status: 400 },
+    );
   }
   return {  errors: { linkCode: null, discordCode: null }};
 };
